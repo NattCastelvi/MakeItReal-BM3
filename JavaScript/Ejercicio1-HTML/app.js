c6e93d8c13ec9c8f36dd60862cbd8f6c5d7e973b
Natt Castelvi <nataliaocastelvi@gmail.com>
2023-06-09 19:58:47 -0500

    Creación de ejercicios de JavaScript

--- JavaScript/Ejercicio1-HTML/app.js
+++ JavaScript/Ejercicio1-HTML/app.js
@@ -1,3 +1,10 @@
+
+
 //alert("Hola desde Make It Real BM3");
 
 console.log("¡Hola Mundo!");
+console.log('Este es "la frase" de la \'linea\' 6');
+console.log("Este es 'la frase' de la \"línea\" 7");
+
+//Concatenar String
+console.log("Hola "+ "desde " + "mi " + "pc");
