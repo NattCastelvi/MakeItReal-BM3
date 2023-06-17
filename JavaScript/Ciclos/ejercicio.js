42f9dbbdf02b15f386a9a9a3585671f29e89c871
Natt Castelvi <nataliaocastelvi@gmail.com>
2023-06-17 04:55:10 -0500

    ciclos

---
+++ JavaScript/Ciclos/ejercicio.js
@@ -0,0 +1,20 @@
+//Imprimir los números  del 10 a 20 pero de 2 en 2.
+
+/* 
+Inicializar i= 10.
+Condición: i menor o igual a 20.
+Incrementador sume de a '2'.
+Cuerpo: Imprimir en la terminal el valor
+*/
+
+for (let i = 10; i <= 20; i = i + 2) {
+    console.log("Número: " + i);
+}
+
+/// WHILE
+let i = 20;
+
+ while (i >= 10){
+    console.log(i);
+    i = i - 2; 
+}
