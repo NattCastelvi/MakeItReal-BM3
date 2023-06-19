118fbe71c5c9c9bbddc8adeff56fc3b4061bb51f
Natt Castelvi <nataliaocastelvi@gmail.com>
2023-06-18 19:47:20 -0500

    Solucion misión 7

---
+++ JavaScript/Misión 7/corriguiendo-posiciones.js
@@ -0,0 +1,15 @@
+let arr = ["Primero", "Segundo", "Tercero", "Cuarto", "Soy el último elemento"];
+let newArray = ["Quinto", "Sexto"];
+
+// Obtén el índice donde se debe insertar el nuevo elemento
+let insertIndex = arr.length - 1;
+
+// Inserta los elementos de newArray en arr uno por uno
+for (let i = 0; i < newArray.length; i++) {
+  arr.splice(insertIndex + i, 0, newArray[i]);
+}
+
+// Imprime cada elemento en la consola
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
---
+++ JavaScript/Misión 7/suma.js
@@ -0,0 +1,11 @@
+function sum(num1,num2) {
+    return num1 + num2
+}
+
+console.log(sum(10, 20)); // 30
+
+console.log(sum(10, 10)); // 20
+
+console.log(sum(40, 60)); // 100
+
+console.log(sum(50, 200)); // 250
